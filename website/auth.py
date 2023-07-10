2d17f397769e52adcd972c3e870ba9f2d435c48a
Harshitkharkwal <112532153+Harshitkharkwal@users.noreply.github.com>
2023-07-10 15:25:40 +0530

    Add files via upload

---
+++ website/auth.py
@@ -0,0 +1,64 @@
+from flask import Blueprint,render_template,request,flash,redirect,url_for #like aler in js
+from .models import User
+from werkzeug.security import generate_password_hash,check_password_hash
+from .import db
+from flask_login import  login_user,login_required,logout_user,current_user
+auth=Blueprint('auth',__name__)
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+          email=request.form.get('email')
+          password=request.form.get('password')
+          user=User.query.filter_by(email=email).first()
+
+          if user:
+              if check_password_hash(user.password,password):
+                  flash('Logged in successfully!',category='success')
+                  login_user(user,remember=True)
+                  return redirect(url_for('views.home'))
+              else:
+                  flash('Incorrect Password or Email,try again',category='error')
+          else:
+              flash('Email does not exist',category='error')
+               
+                  
+   
+    return render_template('login.html',user=current_user)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+@auth.route('/signup',methods=['GET','POST'])
+def signup():
+    if request.method=='POST':
+        email=request.form.get('email')
+        name=request.form.get('name')
+        password1=request.form.get('password1')
+        password2=request.form.get('password2')
+        user=User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email already exists.',category='error')
+
+        elif(len(email)<4):
+            flash("Hey!your email is too short",category='error')
+        elif(len(name)<2):
+            flash("Hey!firstName must be greater than 2 character",category='error')
+        elif(password1!=password2):
+            flash("Password don't match",category='error')
+        elif(len(password1)<7):
+            flash("Hey!your password is too short",category='error')
+        else:
+            new_user=User(email=email,name=name,password=generate_password_hash(password1,method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            # user=User.query.filter_by(email=email).first()
+
+            flash('Account Created',category='success')
+            return redirect(url_for('views.home'))
+            #add user to database
+    return render_template('signup.html',user=current_user)
